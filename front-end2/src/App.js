08de98da047f86fa595e6b0271e0ca1eefc28011
sandyvmarques <aluno19997@ipt.pt>
2019-03-17 22:41:12 +0000

    Alteracao dos nomes

--- front-end2/src/App.js
+++ front-end2/src/App.js
@@ -10,22 +10,22 @@ class App extends Component {
         <div className="App__Form">
           <div className="PageSwitcher">
             <a href="#" className="PageSwitcher__Item ">
-              Sign In
+              Iniciar Sessao
             </a>
             <a
               href="#"
               className="PageSwitcher__Item PageSwitcher__Item--Active"
             >
-              Sign Up
+              Registo
             </a>
           </div>
           <div className="FormTitle">
             <a href="#" className="FormTitle__Link ">
-              Sign In
+              Iniciar Sessao
             </a>{" "}
             or{" "}
             <a href="#" className="FormTitle__Link FormTitle__Link --Active">
-              Sign Up
+              Registo
             </a>
           </div>
 
@@ -33,56 +33,56 @@ class App extends Component {
             <form className="FormField " onSubmit={this.handleSubmit}>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="name">
-                  Full Name
+                  Nome Completo
                 </label>
                 <input
                   type="text"
                   id="name"
                   className="FormField__Input"
-                  placeholder="Enter your full name"
+                  placeholder="Insira o seu nome completo"
                   name="name"
                 />
               </div>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="email">
-                  Email Address
+                  Email
                 </label>
                 <input
                   type="Confpasemailsword"
                   id="email"
                   className="FormField__Input"
-                  placeholder="Enter a email"
+                  placeholder="Insira o seu email"
                   name="email"
                 />
               </div>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="password">
-                  Password
+                  Palavra-Chave
                 </label>
                 <input
                   type="password"
                   id="password"
                   className="FormField__Input"
-                  placeholder="Enter a password"
+                  placeholder="Insira a sua palavra-chave"
                   name="password"
                 />
               </div>
               <div className="FormField">
                 <label className="FormField__Label" htmlFor="Confpassword">
-                  Confirmation Password
+                  Confirmacao da Palavra-Chave
                 </label>
                 <input
                   type="Confpassword"
                   id="Confpassword"
                   className="FormField__Input"
-                  placeholder="Enter a Confpassword"
+                  placeholder="Insira novamente sua palavra-chave."
                   name="Confpassword"
                 />
               </div>
               <div className="FormField">
-                <button className="FormField__Button mr-20">Sign Up</button>
+                <button className="FormField__Button mr-20">Registar-se</button>
                 <a href="#" className="FormField__Link">
-                  Im already a member
+                  Ja sou membro
                 </a>
               </div>
             </form>
