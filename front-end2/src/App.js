5baebec106b3cb47e2285a5e84448dd2aedef3ad
sandyvmarques <aluno19997@ipt.pt>
2019-03-17 21:06:33 +0000

    Criacao dos botoes Sign up e Sign in

--- front-end2/src/App.js
+++ front-end2/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello world</div>;
+    return (
+      <div className="App">
+        <div className="App--Aside" />
+        <div className="App--Form">
+          <div className="PageSwitcher">
+            <a href="#" className="PageSwitcher__Item ">
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="PageSwitcher__Item PageSwitcher__Item--Active"
+            >
+              Sign Up
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
